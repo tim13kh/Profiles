25df4b5267d37b92e462e40c7ffc8c09b0548d77
Temir Makhmudov <temir.makhmudov@fisglobal.com>
2018-03-02 21:13:45 -0500

    syslog update

--- rsyslog2.sh
+++ rsyslog2.sh
@@ -4,6 +4,8 @@ rsys_file="/etc/rsyslog.conf"
 sys_file="/etc/syslog.conf"
 rline="*.info @be-siem-01.clear2pay.com:514"
 line="*.info @be-siem-01.clear2pay.com"
+suse_line='destination loghost {udp("10.58.231.83" port(514));};'
+suse_line2='log {source(src);destination(loghost);};'
 
 
 rhel_centos (){
@@ -54,14 +56,32 @@ debian_ubuntu (){
    fi
 }
 
+suse(){
+   if [ -e "/etc/syslog-ng/syslog-ng.conf" ];
+   then
+      if grep -qFx "$suse_line" "/etc/syslog-ng/syslog-ng.conf" && grep -qFx "$suse_line2" "/etc/syslog-ng/syslog-ng.conf";
+      then
+         echo "Line is alredy present in /etc/syslog-ng/syslog-ng.conf"
+      else
+         echo "$suse_line" >> "/etc/syslog-ng/syslog-ng.conf"
+         echo "$suse_line2" >> "/etc/syslog-ng/syslog-ng.conf"
+         echo "line has been added to /etc/syslog-ng/syslog-ng.conf"
+         echo "restarting rsyslog service..."
+         service syslog restart
+         echo "OK"
+      fi
+   else
+      echo "File Doesn't exist"
+   fi
+}
+
 
 DISTRO=$( cat /etc/*release | tr [:upper:] [:lower:] | grep -Poi -m 1 '(debian|ubuntu|red hat|centos|suse|oracle)' | uniq )
 
 case $DISTRO in
-     centos|'red hat')  rhel_centos;;
-     debian|ubuntu)     debian_ubuntu;;
-     suse)              echo "it is suse";;
-     oracle)            echo "it is oracle";;
-     *)                 echo "Unknow Distro";;
+     centos|'red hat'|oracle)  rhel_centos;;
+     debian|ubuntu)            debian_ubuntu;;
+     suse)                     suse;;
+     *)                        echo "Unknow Distro";;
 esac
 
